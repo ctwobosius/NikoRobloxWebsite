8c9dc14089cbd42705658e7078362a047b8a059d
nikocoder <hardingdavidjames@gmail.com>
2022-01-09 11:07:24 -0800

    feenish queez

--- docs/js/Gen.js
+++ docs/js/Gen.js
@@ -3,8 +3,8 @@ var vw = document.getElementById('voweled word');
 var gen = document.getElementById('gen');
 
 function on_click() {
-    alert(vw.value.replace(/[aiuAIU]/g, ""));
-
+    alert(vw.value.replace(/[aiuAIUzZrhJ]/g, ""));
+    
 }
 
 gen.onclick = on_click;
